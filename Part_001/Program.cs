0b86dc411a39335b074d6ba87932d86a135a7386
Danil <danil3rain@mail.ru>
2022-12-04 00:47:50 +0200

    задача 1 решина

--- Part_001/Program.cs
+++ Part_001/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(" 123Hello, World!");
+﻿Console.Write("Введите первое число: ");         
+int a = int.Parse (Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = int.Parse (Console.ReadLine());
+if (a>b)
+    {
+        Console.Write ( "Это число больше: " );
+        Console.WriteLine ( a );
+        Console.Write ( "Это число Меньше: " );
+        Console.WriteLine ( b );
+    }
+    else
+    {
+        Console.Write ( "Это число больше: " );
+        Console.WriteLine ( b );
+        Console.Write ( "Это число больше: " );
+        Console.WriteLine ( a );
+    }
+
+
+
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
