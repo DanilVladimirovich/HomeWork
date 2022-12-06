af2fc7a38bd1592ebcaf6fbedad4d619f72c7f50
Danil <danil3rain@mail.ru>
2022-12-06 14:40:21 +0200

    задачи решены

--- Part_006_Trete_chislo/Part_006_Trete_chislo/Program.cs
+++ Part_006_Trete_chislo/Part_006_Trete_chislo/Program.cs
@@ -1,17 +1,39 @@
 ﻿Console.Write("Введите значение: ");
 int i = int.Parse (Console.ReadLine());
-if (i <= 99)
+int j = 10;
+if (i > 99 || i < -99)
 {
-    Console.WriteLine("нет третьего символа: ");
+    while (i > 999 || i < -999)
+   {
+       i = i / j;
+    }
+    i = i % 10;
+    Console.WriteLine(i);
 }
 else
-{ 
-string s1 = i.ToString();
-    Console.WriteLine(s1[2]);
-}
-
+   Console.WriteLine("нет третьего значения: ");
+    
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
+
+
+//Console.Write("Введите значение: ");
+//int i = int.Parse(Console.ReadLine());
+//if (Math.Abs(i) <= 99)
+//{
+  //Console.WriteLine("нет третьего символа: ");
+//}
+//else
+//{
+  //string s1 = i.ToString();
+
+    //if (i > 0)
+     //Console.WriteLine(s1[2]);
+     //
+    //else
+       // Console.WriteLine(s1[3]);
+//}
+
---
+++ Part_007/Part_007/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число равное дню недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7)
+    Console.WriteLine($"{day} Выходной");
+else if (day == 5 || day == 4 || day == 3 || day == 2 || day == 1)
+    Console.WriteLine($"{day} Рабочий день");
+else Console.WriteLine("Erorr");
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
