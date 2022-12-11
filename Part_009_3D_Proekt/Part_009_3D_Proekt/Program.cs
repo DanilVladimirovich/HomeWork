d6643d0747d29b71cd1994a5a6fe3e053783fafa
Danil <danil3rain@mail.ru>
2022-12-11 13:24:03 +0200

    решение с 3д проекцией

--- Part_009_3D_Proekt/Part_009_3D_Proekt/Program.cs
+++ Part_009_3D_Proekt/Part_009_3D_Proekt/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿double GetNumber(string message) 
+{ 
+    Console.WriteLine(message); 
+    double number = double.Parse(Console.ReadLine()!); 
+    return number; 
+}
+double ax = GetNumber("Введите x1 "); 
+double ay = GetNumber("Введите y1 ");
+double az = GetNumber("Введите z1 ");
+double bx = GetNumber("Введите x2 "); 
+double by = GetNumber("Введите y2 ");
+double bz = GetNumber("Введите z2 ");
+
+double ab = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+
+Console.WriteLine(ab);
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B(2, 1, -7), -> 15.84
+//A(7, -5, 0); B(1, -1, 9)-> 11.53
