c5d0f6e73b0a36548e82f0f6c3afb795a2a5fabe
Danil <danil3rain@mail.ru>
2022-12-11 16:01:27 +0200

    решил задачу с палиндромом

--- Part_007_palindrom/Part_007_palindrom/Program.cs
+++ Part_007_palindrom/Part_007_palindrom/Program.cs
@@ -1,24 +1,20 @@
-﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿Console.Write("Введите число для проверки палиндрома: ");
+string number = Console.ReadLine();
+string numberTwo = string.Empty;
+for (int i = number.Length - 1; i >= 0; i--)
+{ 
+numberTwo+= number[i].ToString();  
+}
+if (number == numberTwo)
+{
+    Console.WriteLine($"{number} - Палиндром");
+}
+else 
+{
+    Console.WriteLine($"{number} - Не является палиндромом");
+}
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 //14212 -> нет
 //12821 -> да
 //23432 -> да
-
-Console.Write("Введите число для проверки палиндрома: ");
-string number = Console.ReadLine();
-     int b = number.Length-1;
-for (int i = 0; i < number.Length; i++)
-{
-        if (number[i] == number[b])
-    { 
-        b = b-1;
-    }      
-    else
-    { 
-        Console.WriteLine($"{number} Не является полиндромо");
-    break;
-    }
-    Console.WriteLine("является полиндром");
-}
-
-
-
