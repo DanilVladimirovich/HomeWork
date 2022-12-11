b5a1de616879f8a66c127dd84fe2af861fa522b5
Danil <danil3rain@mail.ru>
2022-12-11 20:15:09 +0200

    Решение задачи на массив

---
+++ Part_013_Vivod_Massiva/Part_013_Vivod_Massiva/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] GetArray(int length) 
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt();
+    }
+    return array;
+}
+string GetArrayAsString(int[] array)
+{
+    string s = string.Empty;
+    foreach (var item in array) { s += $"[{item}] "; }
+    return s;
+}
+
+int ReadInt()
+{
+    Console.WriteLine("Введите число: ");
+
+    if (int.TryParse(Console.ReadLine(), out int i)) 
+    {
+        return i;
+    }
+
+    Console.WriteLine("Это не число: ");
+
+    return -1;
+}
+
+int[] massiv = GetArray(8); 
+Console.WriteLine(GetArrayAsString(massiv));
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33-> [6, 1, 33]
+
+
